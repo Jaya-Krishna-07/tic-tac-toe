f17e90bb0bac9eeb4a3facfa700f9de22e50aeff
Jaya Krishna <jayamannem07@gmail.com>
2024-11-06 12:30:00 +0530

    Display status: who is the next player and winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,20 +2,53 @@ import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
-
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  
+
   const handleClick = (i) => {
-    if (squares[i]) {return;}
+    if (squares[i] || calculateWinners(squares)) {
+      return;
+    }
+
     const nextSquares = squares.slice();
+
     if (xIsNext) nextSquares[i] = "X";
     else nextSquares[i] = "O";
+
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   };
+
+  const calculateWinners = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  
+  const winner = calculateWinners(squares);
+  let status;
+
+  if (winner) { status = "Winner: " + winner; }
+  else status = "Next Player: " + (xIsNext ? "X" : "O");
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
